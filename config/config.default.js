6d3d4cf20f3343e911964001f6ec1413829eea29
jackiecookie <2711002357@qq.com>
2017-08-12 23:27:11 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1502524495070_1690';
+
+  // add your config here
+
+  return config;
+};
